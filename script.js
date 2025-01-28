8d6280e7b44ac9e889f479034d38b19d3c9f7be7
tylerwood26 <24847346+tylerwood26@users.noreply.github.com>
2025-01-28 10:47:16 -0500

    Basic script setup

---
+++ script.js
@@ -0,0 +1,32 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+const numberOne = null;
+const operator = null;
+const numberTwo = null;
+
+function operate(num1, operator, num2) {
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case 'subtract':
+            return subtract(num1, num2);
+    }
+}
