1a2f01733eb2e1f1f1248ce4d6ae4326245f0115
tylerwood26 <24847346+tylerwood26@users.noreply.github.com>
2025-02-04 13:20:26 -0500

    Add basic calculator tasks

--- script.js
+++ script.js
@@ -14,9 +14,9 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
-const numberOne = null;
-const operator = null;
-const numberTwo = null;
+let numberOne = '';
+let operator;
+let numberTwo = '';
 
 function operate(num1, operator, num2) {
     switch (operator) {
@@ -26,7 +26,43 @@ function operate(num1, operator, num2) {
             return subtract(num1, num2);
         case '*':
             return multiply(num1, num2);
-        case 'subtract':
-            return subtract(num1, num2);
+        case '/':
+            return divide(num1, num2);
     }
 }
+
+let numBtns = document.querySelectorAll('.num-btn');
+let operatorBtns = document.querySelectorAll('.operator-btn');
+let display = document.querySelector('.display');
+let equalsBtn = document.querySelector('.equals-btn');
+let clearBtn = document.querySelector('.clear-btn');
+
+numBtns.forEach(updateDisplay);
+operatorBtns.forEach(updateDisplay);
+
+function updateDisplay(element) {
+    element.addEventListener("click", (e) => {
+        display.textContent += e.target.textContent;
+        if (e.target.className === 'num-btn' && operator === undefined) {
+            numberOne += e.target.textContent;
+            console.log(`numberOne: ${numberOne}`);
+        } else if (e.target.className === 'operator-btn') {
+            operator = e.target.textContent;
+            console.log(`operator: ${operator}`);
+        } else if (e.target.className === 'num-btn' && operator !== undefined) {
+            numberTwo += e.target.textContent;
+            console.log(`numberTwo: ${numberTwo}`);
+        }
+    })
+}
+
+clearBtn.addEventListener("click", () => {
+    display.textContent = '';
+    numberOne = '';
+    operator = undefined;
+    numberTwo = '';
+});
+
+equalsBtn.addEventListener("click", () => {
+    display.textContent = operate(Number(numberOne), operator, Number(numberTwo));
+});
